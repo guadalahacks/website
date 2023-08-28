0b656c9a6e2f40da5d69323c69c72a899419decc
Han Rodríguez <alejandro.tzaax@gmail.com>
2023-08-28 01:09:36 -0700

    format code of conduct

--- frontend/src/pages/cdc.tsx
+++ frontend/src/pages/cdc.tsx
@@ -6,42 +6,60 @@ import App from '@/layouts/App';
 const CodeOfConduct: React.FC = () => {
     return (
       <App>
-      <div className="coc-main-container">
         <div className="code-of-conduct-container">
-        Código de Conducta del Hackathon
-        Nuestro compromiso
-        El objetivo principal de este Hackathon es fomentar la innovación, la colaboración y la creación de soluciones tecnológicas entre todos los participantes. Estamos comprometidos a proporcionar un ambiente amigable, seguro y acogedor para todos, sin importar género, orientación sexual, discapacidad, etnicidad, religión, o cualquier otra categoría de diversidad. Este Código de Conducta esboza nuestras expectativas para el comportamiento de los participantes, así como las consecuencias para un comportamiento inaceptable.
+          <h1>Código de conducta del guadala<b>hacks</b></h1>
+          <h2>Nuestro compromiso</h2>
+          <p>
+            El objetivo de guadala<b>hacks</b> es fomentar la innovación y la creación de soluciones tecnológicas en un espacio de construcción, colaboración y aprendizaje.
+            Estamos comprometidxs a proporcionar un ambiente amigable, seguro y acogedor para todxs, para todo género, orientación sexual, discapacidad, etnia, religión, o cualquier otra categoría de diversidad.
+            Este código de conducta especifica los lineamientos de comportamiento de los participantes, así como las consecuencias de un comportamiento inaceptable.
+          </p>
 
-        Comportamiento esperado
-        Participa de manera auténtica y activa.
-        Ejercer consideración y respeto en tus palabras y acciones.
-        Evita cualquier comentario o conducta discriminatoria, ofensiva o violenta.
-        Respeto a los límites personales y al espacio personal de otros.
-        Estar atento a lo que dicen tus compañeros participantes y hacer tu mejor esfuerzo para fomentar una experiencia segura y positiva para todos.
-        Respetar las reglas y directrices del lugar y del evento.
-        Comportamiento inaceptable
-        El acoso, el uso de lenguaje ofensivo o imágenes sexualizadas.
-        Intimidación, stalking o seguimiento.
-        Fotografías o grabaciones sin el consentimiento explícito de los individuos.
-        Interrupción sostenida de charlas u otros eventos.
-        Contacto físico inapropiado o indeseado.
-        Intimidación o bullying, ya sea en línea o fuera de línea.
-        Cualquier otro tipo de comportamiento considerado inapropiado en un entorno profesional.
-        Consecuencias de un comportamiento inaceptable
-        Los organizadores del Hackathon tienen el derecho y la responsabilidad de remover, editar o rechazar comentarios, ediciones, código, ediciones en wiki, problemas y otras contribuciones que no estén alineadas con este Código de Conducta. Además, pueden tomar acciones para sancionar o expulsar temporal o permanentemente a cualquier participante de cualquier comportamiento que consideren inapropiado, amenazante, ofensivo o dañino.
+          <h2>Comportamiento esperado</h2>
+          <ul>
+            <li>Participar de manera auténtica y activa.</li>
+            <li>Ejercer consideración y respeto en tus palabras y acciones.</li>
+            <li>Evitar cualquier comentario o conducta discriminatoria, ofensiva o violenta.</li>
+            <li>Respetar los límites personales y el espacio personal de otrxs.</li>
+            <li>Estar atento a lo que dicen tus compañeros participantes y hacer tu mejor esfuerzo para fomentar una experiencia segura y positiva para todos.</li>
+            <li>Respetar las reglas y directrices del lugar y del evento.</li>
+          </ul>
+          
+          <h2>Comportamiento inaceptable</h2>
+          <ul>
+            <li>El acoso, el uso de lenguaje ofensivo o imágenes sexualizadas.</li>
+            <li>Intimidación, stalking o seguimiento.</li>
+            <li>Fotografías o grabaciones sin el consentimiento explícito de los individuos.</li>
+            <li>Interrupción sostenida de charlas u otros eventos.</li>
+            <li>Contacto físico inapropiado o indeseado.</li>
+            <li>Intimidación o bullying, ya sea en línea o en persona.</li>
+            <li>Cualquier otro tipo de comportamiento considerado inapropiado en un entorno profesional.</li>
+          </ul>
 
-        Si eres testigo o eres víctima de un comportamiento inaceptable
-        Si eres víctima o testigo de un comportamiento inaceptable, o tienes alguna otra inquietud, por favor comunícate con un miembro del equipo organizador lo antes posible. El equipo del Hackathon estará disponible para ayudar a los participantes a contactar a la seguridad del lugar o a las autoridades locales, para proporcionar escoltas, o para ayudar de cualquier otra forma a aquellos que experimenten un comportamiento inaceptable para que se sientan seguros durante el evento.
+          <h2>Consecuencias de un comportamiento inaceptable</h2>
+          <p>
+            Lxs organizadores del guadala<b>hacks</b> tienen el derecho y la responsabilidad de sancionar o expulsar temporal o permanentemente a cualquier participante por cualquier comportamiento que consideren inapropiado, amenazante, ofensivo o dañino.
+          </p>
 
-        Respeto al entorno
-        Recordamos a los participantes que deben respetar las instalaciones y su entorno, así como cualquier equipo y recurso proporcionado como parte del Hackathon.
+          <h2>Si eres testigo o eres víctima de un comportamiento inaceptable</h2>
+          <p>
+            Si eres víctima o testigo de un comportamiento inaceptable, o tienes alguna otra inquietud, por favor comunícate con un miembro del equipo de guadala<b>hacks</b> lo antes posible o escríbenos a <a href="mailto:incidentes@guadalahacks.com">incidentes@guadalahacks.com</a>.
+          </p>
 
-        Contacto
-        Para reportar problemas relacionados con el código de conducta, contacta directamente al equipo organizador. Se tratará toda la información con la mayor discreción posible.
+          <h2>Respeto al entorno</h2>
+          <p>
+            Recordamos a los participantes que deben respetar las instalaciones y su entorno, así como cualquier equipo y recurso proporcionado como parte del hackatón.
+          </p>
 
-        Apreciamos tu colaboración en hacer de este Hackathon un lugar amigable y acogedor para todos los participantes.
+          <h2>Contacto</h2>
+          <p>
+            Para reportar problemas relacionados con el código de conducta, comunícate con un miembro del equipo de guadala<b>hacks</b> o escríbenos a <a href="mailto:incidentes@guadalahacks.com">incidentes@guadalahacks.com</a>.
+          </p>
+
+          <p>
+            Apreciamos tu colaboración en hacer de este guadala<b>hacks</b> un lugar amigable y acogedor para todxs lxs participantes.
+          </p>
         </div>
-      </div>
       </App>
     );
   };
