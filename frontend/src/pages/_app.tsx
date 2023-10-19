12b3fcacaa77f793928800ce894f52d12e1b63ca
Dalia Sofía Magallón Páramo <magallon.paramo@gmail.com>
2023-10-19 06:03:57 -0600

    Fix favicon

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="/icon.ico" />
+       <link rel="shortcut icon" type="image/x-icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/favicon.ico" />
         <title>GuadalaHacks</title>
         <meta name="description" content="El hackatón estudiantil del occidente de México" />
         <meta name="keywords" content="hackathon, guadalajara, guadalahacks, hack, coding, programming, programación, hackatón, hackaton, hackathon, student, computacion, estudiante" />
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+        <link rel="shortcut icon" type="image/x-icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/favicon.ico" />
         <meta name="description" content="El hackatón estudiantil del occidente de México" />
         <meta name="keywords" content="hackathon, guadalajara, guadalahacks, hack, coding, programming, programación, hackatón, hackaton, hackathon, student, computacion, estudiante" />
         <meta name="og:image" content="/banner.png" />
