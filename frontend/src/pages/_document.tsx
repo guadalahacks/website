cfd73b4ed1e2ab57660e7539efcf8c9c8b26d9d3
Dalia Sofía Magallón Páramo <magallon.paramo@gmail.com>
2023-10-19 05:56:59 -0600

    Rename favicon, add type metadata

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/icon.ico" />
+        <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
         <meta name="description" content="El hackatón estudiantil del occidente de México" />
         <meta name="keywords" content="hackathon, guadalajara, guadalahacks, hack, coding, programming, programación, hackatón, hackaton, hackathon, student, computacion, estudiante" />
         <meta name="og:image" content="/banner.png" />
