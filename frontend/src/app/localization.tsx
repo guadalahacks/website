b8eaccbc433f9393c29a72617669080935100f56
han rodríguez <han@gonzalpi.com>
2024-10-26 20:40:33 -0700

    add empty strings and elements for english

--- frontend/src/app/localization.tsx
+++ frontend/src/app/localization.tsx
@@ -7,25 +7,32 @@ export const stringLocalization: StringLocalization = {
     // }
 
     partners_github_wording: {
-        es: "Aprende sobre GitHub Copilot"
+        es: "Aprende sobre GitHub Copilot",
+        en: ""
     },
     partners_emprendimiento_alt: {
-        es: "Emprendimiento TEC"
+        es: "Emprendimiento TEC",
+        en: ""
     },
     partners_eic_alt: {
-        es: "Escuela de Ingeniería y Ciencias del Tecnológico de Monterrey"
+        es: "Escuela de Ingeniería y Ciencias del Tecnológico de Monterrey",
+        en: ""
     },
     partners_gce_wording: {
-        es: "GitHub Student Developer Pack"
+        es: "GitHub Student Developer Pack",
+        en: ""
     },
     partners_blend_wording: {
-        es: "Explora las vacantes"
+        es: "Explora las vacantes",
+        en: ""
     },
     gallery_photo_alt: {
-        es: "Colección de fotografías de Guadalahacks 2024, personas programando y recibiendo premios"
+        es: "Colección de fotografías de Guadalahacks 2024, personas programando y recibiendo premios",
+        en: ""
     },
     partnerLogo_defaultWording: {
-        es: "Enlace"
+        es: "Enlace",
+        en: ""
     }
 }
 
@@ -36,46 +43,56 @@ export const elementLocalization: ElementLocalization = {
     // }
 
     faq_title: {
-        es: <>FAQ</>
+        es: <>FAQ</>,
+        en: <></>
     },
     faq_q1: {
-        es: <>¿Qué es guadala<b>hacks</b>?</>
+        es: <>¿Qué es guadala<b>hacks</b>?</>,
+        en: <></>
     },
     faq_q1_answer: {
         es: <>
             Únete a cientos de estudiantes de todo México para un fin de semana de construcción, aprendizaje y diversión.
             Convive y conecta con la comunidad <i>tech</i> de México, desarrolla tu proyecto y pasa un fin de semana inolvidable.
             Conoce Guadalajara y el Tecnológico de Monterrey, universidad líder en México y Latinoamérica.
-        </>
+        </>,
+        en: <></>
     },
     faq_q2: {
-        es: <>¿Quiénes son bienvenidxs?</>
+        es: <>¿Quiénes son bienvenidxs?</>,
+        en: <></>
     },
     faq_q2_answer: {
         es: <>
             Puedes formar parte del evento si eres estudiante o exatec, con menos de un año de haber egresado,
             de cualquier campus del Tecnológico de Monterrey, no importa tu carrera ni nivel de experiencia o de estudios.
             Somos un evento inclusivo y diverso, y fomentamos un espacio seguro para todxs.
-        </>
+        </>,
+        en: <></>
     },
     faq_q3: {
-        es: <>¿Cuánto cuesta?</>
+        es: <>¿Cuánto cuesta?</>,
+        en: <></>
     },
     faq_q3_answer: {
         es: <>
             La participación no tiene costo alguno.
             Habrá comida gratis durante todo el evento, así como premios para los mejores proyectos.
             ¡No te lo puedes perder!
-        </>
+        </>,
+        en: <></>
     },
     faq_q4: {
-        es: <>¿Qué necesito llevar?</>
+        es: <>¿Qué necesito llevar?</>,
+        en: <></>
     },
     faq_q4_answer: {
-        es: <>Trae tu computadora, cargador, cepillo de dientes, ropa cómoda y muchas ganas de aprender y divertirte.</>
+        es: <>Trae tu computadora, cargador, cepillo de dientes, ropa cómoda y muchas ganas de aprender y divertirte.</>,
+        en: <></>
     },
     faq_q5: {
-        es: <>¿Cuántas personas deben conformar un equipo?</>
+        es: <>¿Cuántas personas deben conformar un equipo?</>,
+        en: <></>
     },
     faq_q5_answer: {
         es: <>
@@ -83,138 +100,182 @@ export const elementLocalization: ElementLocalization = {
             Se recomiendan equipos de 2 a 4 integrantes, pero está permitido participar individualmente.
             Todxs lxs integrantes deben registrarse individualmente.
             Registrarán su equipo durante el evento.
-        </>
+        </>,
+        en: <></>
     },
     faq_q6: {
-        es: <>¿Qué clase de proyecto debería desarrollar?</>
+        es: <>¿Qué clase de proyecto debería desarrollar?</>,
+        en: <></>
     },
     faq_q6_answer: {
         es: <>
             Los retos serán liberados durante el evento.
             No está permitido trabajar en proyectos que ya hayan sido desarrollados previamente.
-        </>
+        </>,
+        en: <></>
     },
     faq_more: {
-        es: <>Más información próximamente.</>
+        es: <>Más información próximamente.</>,
+        en: <></>
     },
     landing_title: {
-        es: <>guadala<b>hacks</b></>
+        es: <>guadala<b>hacks</b></>,
+        en: <></>
     },
     landing_seeYouSoon: {
-        es: <>¡Nos vemos en 2025!</>
+        es: <>¡Nos vemos en 2025!</>,
+        en: <></>
     },
     landing_thankYou: {
-        es: <>Muchas gracias por acomapañarnos.</>
+        es: <>Muchas gracias por acomapañarnos.</>,
+        en: <></>
     },
     landing_teamsComingSoon: {
-        es: <>Pronto podrás conocer a los equipos detrás de los asombrosos proyectos.</>
+        es: <>Pronto podrás conocer a los equipos detrás de los asombrosos proyectos.</>,
+        en: <></>
     },
     landing_seeProjects: {
-        es: <>Conoce los proyectos</>
+        es: <>Conoce los proyectos</>,
+        en: <></>
     },
     partners_title: {
-        es: <>Partners</>
+        es: <>Partners</>,
+        en: <></>
     },
     schedule_title: {
-        es: <>Agenda</>
+        es: <>Agenda</>,
+        en: <></>
     },
     schedule_table_date: {
-        es: <>Fecha</>
+        es: <>Fecha</>,
+        en: <></>
     },
     schedule_table_date_0518: {
-        es: <>18 de mayo</>
+        es: <>18 de mayo</>,
+        en: <></>
     },
     schedule_table_date_0519: {
-        es: <>19 de mayo</>
+        es: <>19 de mayo</>,
+        en: <></>
     },
     schedule_table_time: {
-        es: <>Hora</>
+        es: <>Hora</>,
+        en: <></>
     },
     schedule_table_time_tbd: {
-        es: <>Por anunciar</>
+        es: <>Por anunciar</>,
+        en: <></>
     },
     schedule_table_event: {
-        es: <>Evento</>
+        es: <>Evento</>,
+        en: <></>
     },
     schedule_table_event_registration: {
-        es: <>Registro</>
+        es: <>Registro</>,
+        en: <></>
     },
     schedule_table_event_inauguration: {
-        es: <>Inauguración</>
+        es: <>Inauguración</>,
+        en: <></>
     },
     schedule_table_event_lunch: {
-        es: <>Comida</>
+        es: <>Comida</>,
+        en: <></>
     },
     schedule_table_event_dinner: {
-        es: <>Cena</>
+        es: <>Cena</>,
+        en: <></>
     },
     schedule_table_event_breakfast: {
-        es: <>Desayuno</>
+        es: <>Desayuno</>,
+        en: <></>
     },
     schedule_table_event_deadline: {
-        es: <>Cierre de envíos</>
+        es: <>Cierre de envíos</>,
+        en: <></>
     },
     schedule_table_event_closure: {
-        es: <>Cierre</>
+        es: <>Cierre</>,
+        en: <></>
     },
     schedule_table_location: {
-        es: <>Lugar</>
+        es: <>Lugar</>,
+        en: <></>
     },
     schedule_table_location_tecSalud: {
-        es: <>Auditorio TecSalud</>
+        es: <>Auditorio TecSalud</>,
+        en: <></>
     },
     schedule_table_location_negocios: {
-        es: <>Hábitat de Negocios</>
+        es: <>Hábitat de Negocios</>,
+        en: <></>
     },
     contact_title: {
-        es: <>Contáctanos</>
+        es: <>Contáctanos</>,
+        en: <></>
     },
     contact_contactUs: {
-        es: <>Escríbenos a <a href="mailto:hola@guadalahacks.com" className='contact-anchor'>hola@guadalahacks.com</a></>
+        es: <>Escríbenos a <a href="mailto:hola@guadalahacks.com" className='contact-anchor'>hola@guadalahacks.com</a></>,
+        en: <></>
     },
     contact_addressHeader: {
-        es: <>Dirección del evento</>
+        es: <>Dirección del evento</>,
+        en: <></>
     },
     footer_contactUs: {
-        es: <>Conecta</>
+        es: <>Conecta</>,
+        en: <></>
     },
     footer_contactUs_email: {
-        es: <>Correo electrónico</>
+        es: <>Correo electrónico</>,
+        en: <></>
     },
     footer_resources: {
-        es: <>Recursos</>
+        es: <>Recursos</>,
+        en: <></>
     },
     footer_resources_cc: {
-        es: <>Código de conducta</>
+        es: <>Código de conducta</>,
+        en: <></>
     },
     footer_resources_joinAsStaff: {
-        es: <>Forma parte del staff</>
+        es: <>Forma parte del staff</>,
+        en: <></>
     },
     footer_resources_joinAsMentor: {
-        es: <>Conviértete en mentor</>
+        es: <>Conviértete en mentor</>,
+        en: <></>
     },
     footer_resources_joinAsSponsor: {
-        es: <>Apóyanos como patrocinador</>
+        es: <>Apóyanos como patrocinador</>,
+        en: <></>
     },
     footer_footnote_madeWith: {
-        es: <>Creado con </>
+        es: <>Creado con </>,
+        en: <></>
     },
     footer_footnote_copyright: {
-        es: <>&copy; 2024 guadalahacks. Todos los derechos reservados.</>
+        es: <>&copy; 2024 guadalahacks. Todos los derechos reservados.</>,
+        en: <></>
     },
     navbar_title: {
-        es: <>guadala<b>hacks</b></>
+        es: <>guadala<b>hacks</b></>,
+        en: <></>
     },
     navbar_home: {
-        es: <>Inicio</>
+        es: <>Inicio</>,
+        en: <></>
     },
     navbar_contact: {
-        es: <>Contacto</>
+        es: <>Contacto</>,
+        en: <></>
     },
     navbar_partners: {
-        es: <>Partners</>
+        es: <>Partners</>,
+        en: <></>
     },
     navbar_faq: {
-        es: <>FAQ</>
+        es: <>FAQ</>,
+        en: <></>
     }
 }
