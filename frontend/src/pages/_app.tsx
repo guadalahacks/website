eec6f702195820552fc94160ef6dafbcb0712ea6
han rodríguez <han@gonzalpi.com>
2024-10-26 17:42:43 -0700

    Lower case hackathon name in header (#6)
    * lower case hack name in header
    
    * update _document.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
        <link rel="shortcut icon" type="image/x-icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/favicon.ico" />
-        <title>GuadalaHacks</title>
+        <title>guadalahacks</title>
         <meta name="description" content="El hackatón estudiantil del occidente de México" />
         <meta name="keywords" content="hackathon, guadalajara, guadalahacks, hack, coding, programming, programación, hackatón, hackaton, hackathon, student, computacion, estudiante" />
         <meta name="og:image" content="/banner.png" />
