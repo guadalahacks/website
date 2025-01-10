bbbe944ae8a1861f85bcb9ee2459aec12323a4e4
Cristobal Medina Meza <a01643121@tec.mx>
2025-01-10 16:24:40 -0600

    Update EventMap.tsx
    Updated EventMap.tsx link to Zoom in when the page is loaded.

--- frontend/src/components/EventMap.tsx
+++ frontend/src/components/EventMap.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import './EventMap.css'
+import './EventMap.css';
 
 const EventMap: React.FC = () => {
-  return (  
+  return (
     <div className="event-map-container">
-      <iframe width="100%" height="100%" src="https://www.openstreetmap.org/export/embed.html?bbox=-103.45780670642854%2C20.731873396153134%2C-103.4543627500534%2C20.73564115922215&amplayer=mapnik&amp;marker=20.732922142462233,-103.45594589239248" ></iframe>
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.openstreetmap.org/export/embed.html?bbox=-103.4570%2C20.7317%2C-103.4550%2C20.7337&layer=mapnik&marker=20.7327,-103.4560"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        title="Event Map"
+      ></iframe>
       <div className="mobile-overlay"></div> {/* Invisible div */}
     </div>
   );
