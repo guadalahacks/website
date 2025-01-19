1bb597ccd3bed32b21d7af804190294f0ef6d4ff
han rodríguez <hi@hanrodz.com>
2025-01-19 02:07:24 -0600

    comment out unconfirmed partners (#22)

--- frontend/src/sections/Partners.tsx
+++ frontend/src/sections/Partners.tsx
@@ -14,13 +14,13 @@ export const Partners: React.FC = () => {
       </h1>
       <div className="partners-logos">
         <PartnerLogo src="./tec.png" alt="Tecnológico de Monterrey" />
-        <PartnerLogo src="./github.png" alt="GitHub" link="https://github.com/features/copilot" wording={getLocalizedString("partners_github_wording")} />
+        {/* <PartnerLogo src="./github.png" alt="GitHub" link="https://github.com/features/copilot" wording={getLocalizedString("partners_github_wording")} /> */}
         <PartnerLogo src="./life.png" alt="LiFE TEC" />
         <PartnerLogo src="./emprendimiento_tec.png" alt={getLocalizedString("partners_emprendimiento_alt")} />
         <PartnerLogo src="./eic.png" alt={getLocalizedString("partners_eic_alt")} />
-        <PartnerLogo src="./wtm_2.png" alt="Women Techmakers" />
-        <PartnerLogo src="./gce.png" alt="GitHub Campus Experts" link="https://education.github.com/benefits?utm_source=2024-04-18-Guadalahacks" wording={getLocalizedString("partners_gce_wording")}/>
-        <PartnerLogo src="./blend.png" alt="Blend" link="https://blend.com/company/careers/emerging-talent/" wording={getLocalizedString("partners_blend_wording")}/>
+        {/* <PartnerLogo src="./wtm_2.png" alt="Women Techmakers" /> */}
+        {/* <PartnerLogo src="./gce.png" alt="GitHub Campus Experts" link="https://education.github.com/benefits?utm_source=2024-04-18-Guadalahacks" wording={getLocalizedString("partners_gce_wording")}/> */}
+        {/* <PartnerLogo src="./blend.png" alt="Blend" link="https://blend.com/company/careers/emerging-talent/" wording={getLocalizedString("partners_blend_wording")}/> */}
       </div>
     </div>
   );
