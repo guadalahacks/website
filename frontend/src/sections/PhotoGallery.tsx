c5b4228e80d278f3637d1f4e3f6b0b52c8795048
han rodríguez <hi@hanrodz.com>
2025-01-19 01:39:49 -0800

    load lang in photo gallery

--- frontend/src/sections/PhotoGallery.tsx
+++ frontend/src/sections/PhotoGallery.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './PhotoGallery.css';
-import { getLocalizedString } from '@/app/locale';
+import { getLocalizedString, languageState } from '@/app/locale';
 
 export const PhotoGallery: React.FC = () => {
+  const [language, setLanguage] = languageState.useState();
   return (
     <div className="gallery-container">
-      <img src="./collage2024.png" alt={getLocalizedString("gallery_photo_alt")} className="single-picture" />
+      <img src="./collage2024.png" alt={getLocalizedString("gallery_photo_alt", language)} className="single-picture" />
     </div>
   );
 };
