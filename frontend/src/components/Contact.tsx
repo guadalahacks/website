064e8b34f08b1cb75d947e728e1e73f0dfe66b6d
han rodríguez <hi@hanrodz.com>
2025-03-29 20:59:43 -0700

    use image in contact

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Contact.scss'
 import { getLocalizedElement, languageState } from '@/app/locale';
-
-
+import Image from 'next/image';
+import img_googlemaps from './../../public/googlemaps.png';
+import img_applemaps from './../../public/applemaps.png';
 
 
 
@@ -17,10 +18,10 @@ const Contact: React.FC = () => {
             <p>Av. Gral. Ramón Corona 2514<br />Colonia Nuevo México, 45201<br />Zapopan, Jal., México</p>
             <div className="maps-links">
                 <a href="https://maps.app.goo.gl/pZdnoWDcySrFbRao6" target="_blank" rel="noopener noreferrer">
-                    <img src="googlemaps.png" alt="Google Maps" />
+                    <Image src={img_googlemaps} alt="Google Maps" />
                 </a>
                 <a href="https://maps.apple.com/?ll=20.733860,-103.456890&q=Instituto%20Tecnologico%20y%20de%20Estudios%20Superiores%20de%20Monterrey&t=m" target="_blank" rel="noopener noreferrer">
-                    <img src="applemaps.png" alt="Apple Maps" />
+                    <Image src={img_applemaps} alt="Apple Maps" />
                 </a>
             </div>
 
