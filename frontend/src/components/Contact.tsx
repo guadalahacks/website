051b045b83aa8e9f0d46e57f75de197a9d5e9b75
Brijeshthummar02 <brijeshthummar02@gmail.com>
2025-03-31 09:35:16 +0530

    Update map links in Contact component to use shortened URLs

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -17,10 +17,10 @@ const Contact: React.FC = () => {
             <p>Tecnológico de Monterrey, Campus Guadalajara</p>
             <p>Av. Gral. Ramón Corona 2514<br />Colonia Nuevo México, 45201<br />Zapopan, Jal., México</p>
             <div className="maps-links">
-                <a href="https://www.google.com/maps/place/H%C3%A1bitat+del+Futuro+de+los+Negocios/@20.7336346,-103.456528,21z/data=!4m6!3m5!1s0x8428a922f44685c3:0x83f5ed7fc747dde8!8m2!3d20.7336933!4d-103.4564891!16s%2Fg%2F11l37857y1!5m1!1e2?entry=tts&g_ep=EgoyMDI1MDMyNS4xIPu8ASoASAFQAw%3D%3D&skid=62df9a2c-5ef2-4c4e-8c47-68bcad04b6be" target="_blank" rel="noopener noreferrer">
+                <a href="https://maps.app.goo.gl/vaAGnbzTkCvCTm2eA" target="_blank" rel="noopener noreferrer">
                     <Image src={img_googlemaps} alt="Google Maps" />
                 </a>
-                <a href="https://maps.apple.com/?address=ll=20.733693,-103.456489&q=Habitat%20del%20Futuro%20de%20los%20Negocios&t=m" target="_blank" rel="noopener noreferrer">
+                <a href="https://maps.apple.com/?ll=20.733693,-103.456489&q=Habitat%20del%20Futuro%20de%20los%20Negocios&t=m" target="_blank" rel="noopener noreferrer">
                     <Image src={img_applemaps} alt="Apple Maps" />
                 </a>
             </div>
