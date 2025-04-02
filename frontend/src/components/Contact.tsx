64b3aadcc8157a43bd094f1ef9a2d8bda4cd1df8
Brijesh Thummar <brijeshthummar02@gmail.com>
2025-04-02 08:46:11 +0530

    Bug: Update map links in Contact component to use specific locations (#43)
    * Update map links in Contact component to use specific locations
    
    * Update map links in Contact component to use shortened URLs

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -17,10 +17,10 @@ const Contact: React.FC = () => {
             <p>Tecnológico de Monterrey, Campus Guadalajara</p>
             <p>Av. Gral. Ramón Corona 2514<br />Colonia Nuevo México, 45201<br />Zapopan, Jal., México</p>
             <div className="maps-links">
-                <a href="https://maps.app.goo.gl/pZdnoWDcySrFbRao6" target="_blank" rel="noopener noreferrer">
+                <a href="https://maps.app.goo.gl/vaAGnbzTkCvCTm2eA" target="_blank" rel="noopener noreferrer">
                     <Image src={img_googlemaps} alt="Google Maps" />
                 </a>
-                <a href="https://maps.apple.com/?ll=20.733860,-103.456890&q=Instituto%20Tecnologico%20y%20de%20Estudios%20Superiores%20de%20Monterrey&t=m" target="_blank" rel="noopener noreferrer">
+                <a href="https://maps.apple.com/?ll=20.733693,-103.456489&q=Habitat%20del%20Futuro%20de%20los%20Negocios&t=m" target="_blank" rel="noopener noreferrer">
                     <Image src={img_applemaps} alt="Apple Maps" />
                 </a>
             </div>
