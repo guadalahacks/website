05cbe5bd692b7d580ff290f2e70eaf203755a057
han rodríguez <hi@hanrodz.com>
2025-05-04 00:14:19 -0600

    Update Partners and Sponsors (#50)
    * add HERE Technologies logo
    
    * add HERE Technologies to website
    
    * add GitHub back to website
    
    * reorder partners
    
    * remove Intel and HP
    
    * remove imports
    
    * add GCE and add link to GH
    
    * use localized strings for GCE and GH
    
    * add style for enlarged partner logos
    
    * highlight HERE logo
    
    * restore highlighter partner logo width on mobile

--- frontend/src/components/PartnerLogo.tsx
+++ frontend/src/components/PartnerLogo.tsx
@@ -6,13 +6,14 @@ interface PartnerLogoProps {
     alt: string;
     link?: string;
     wording?: string;
+    highlight?: boolean;
   }
   
-export const PartnerLogo: React.FC<PartnerLogoProps> = ({ src, alt, link, wording }) => {
+export const PartnerLogo: React.FC<PartnerLogoProps> = ({ src, alt, link, wording, highlight }) => {
   const [language, _] = languageState.useState();
     return (
-      <div className="partner-logo-container">
-        <Image src={src} alt={alt} className="partner-logo" />
+      <div className={highlight ? "partner-logo-container-highlight" : "partner-logo-container"}>
+        <Image src={src} alt={alt} className={highlight ? "partner-logo-highlight" : "partner-logo"} />
         {link && (
           <a href={link} target="_blank" rel="noopener noreferrer" className="partner-link">
             {wording || getLocalizedString("partnerLogo_defaultWording", language)}
--- frontend/src/sections/Partners.tsx
+++ frontend/src/sections/Partners.tsx
@@ -3,9 +3,9 @@ import './Partners.scss';
 import { getLocalizedElement, getLocalizedString, languageState } from '@/app/locale';
 import { Varela_Round } from 'next/font/google';
 import { PartnerLogo } from '../components/PartnerLogo'; // Import the new component
-import img_intel from './../../public/intel.png';
-import img_hp from './../../public/hp.png';
-import img_tec from './../../public/tec.png';
+import img_here from './../../public/here.png';
+import img_github from './../../public/github.png';
+import img_gce from './../../public/gce.png';
 import img_life from './../../public/life.png';
 import img_emprendimiento_tec from './../../public/emprendimiento_tec.png';
 import img_eic from './../../public/eic.png';
@@ -21,8 +21,9 @@ export const Partners: React.FC = () => {
           {getLocalizedElement("sponsors_title", language)}
         </h1>
         <div className="partners-logos">
-          <PartnerLogo src={img_intel} alt="Intel" />
-          <PartnerLogo src={img_hp} alt="HP" />
+          <PartnerLogo src={img_here} alt="HERE Technologies" highlight />
+          <PartnerLogo src={img_github} alt="GitHub" link="https://github.com/features/copilot" wording={getLocalizedString("partners_github_wording", language)} />
+          <PartnerLogo src={img_gce} alt="GitHub Campus Experts" link="https://education.github.com/benefits?utm_source=2024-04-18-Guadalahacks" wording={getLocalizedString("partners_gce_wording", language)}/>
         </div>
       </div>
       <div className={"partners-container " + valeraLight.className}>
@@ -30,9 +31,8 @@ export const Partners: React.FC = () => {
           {getLocalizedElement("partners_title", language)}
         </h1>
         <div className="partners-logos">
-          <PartnerLogo src={img_tec} alt="Tecnológico de Monterrey" />
-          <PartnerLogo src={img_life} alt="LiFE TEC" />
           <PartnerLogo src={img_emprendimiento_tec} alt={getLocalizedString("partners_emprendimiento_alt", language)} />
+          <PartnerLogo src={img_life} alt="LiFE TEC" />
           <PartnerLogo src={img_eic} alt={getLocalizedString("partners_eic_alt", language)} />
         </div>
       </div>
