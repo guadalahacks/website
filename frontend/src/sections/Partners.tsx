e07bbab0c0a6aac90e707e37f091b74f876e3cd6
han rodríguez <hi@hanrodz.com>
2025-05-03 22:33:01 -0700

    remove imports

--- frontend/src/sections/Partners.tsx
+++ frontend/src/sections/Partners.tsx
@@ -5,9 +5,6 @@ import { Varela_Round } from 'next/font/google';
 import { PartnerLogo } from '../components/PartnerLogo'; // Import the new component
 import img_here from './../../public/here.png';
 import img_github from './../../public/github.png';
-import img_intel from './../../public/intel.png';
-import img_hp from './../../public/hp.png';
-import img_tec from './../../public/tec.png';
 import img_life from './../../public/life.png';
 import img_emprendimiento_tec from './../../public/emprendimiento_tec.png';
 import img_eic from './../../public/eic.png';
